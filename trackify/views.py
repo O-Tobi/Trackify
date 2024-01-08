780f1ba96f2a5274208bee7e1c57f019dde4fdf1
unknown <dunjoyebiz2020@gmail.com>
2024-01-08 20:58:07 +0100

    added the templates necessary for registration, authentication, the views.py and respective urls

--- trackify/views.py
+++ trackify/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     pass
+
+def register(request):
+    if request.method == 'POST':
+        firstname = request.POST['first_name'],
+        lastname = request.POST['last_name'],
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmation = request.POST['confirm']
+
+        #check if password matches confirmation
+
+        if password != confirmation:
+            return 
