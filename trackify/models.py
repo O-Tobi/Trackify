d396098b1855e73762ca85aedb0b3b547ae20341
unknown <dunjoyebiz2020@gmail.com>
2024-01-09 20:19:45 +0100

    restructuring the models

--- trackify/models.py
+++ trackify/models.py
@@ -1,21 +1,28 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
-class User(AbstractUser):
-    username = models.CharField(max_length=50, blank=False)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=False, null=False, unique=True)
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     profile_picture = models.ImageField(upload_to='profile_pic/', default='default_profile.jpg')
     occupation = models.CharField(max_length=50, blank=False)
 
+    USERNAME_FIELD = 'email'
+
     def __str__(self):
         return self.username
 
 class Finance(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='finances')
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='finances')    
     category = models.CharField(max_length=50, blank=False, null=False)
     amount = models.IntegerField(blank=False, null=False)
     description = models.TextField(blank=True, null=True)
+    income = models.IntegerField(default=0)
     
     def __str__(self):
         return self.category
+ 
+
+
--- trackify/views.py
+++ trackify/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.db import IntegrityError
+from django.urls import reverse
+from trackify.models import UserProfile, Finance
 
 
 # Create your views here.
@@ -6,16 +12,61 @@ from django.shortcuts import render
 def index(request):
     pass
 
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "widepen/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "widepen/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
 def register(request):
-    if request.method == 'POST':
-        firstname = request.POST['first_name'],
-        lastname = request.POST['last_name'],
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirmation = request.POST['confirm']
+    if request.method == "POST":
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        username = request.POST["username"]
+        email = request.POST["email"]
 
-        #check if password matches confirmation
-
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return 
+            return render(request, "widepen/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(
+                username,
+                email,
+                password, 
+                first_name=firstname, 
+                last_name=lastname
+                )
+            user.save()
+        except IntegrityError:
+            return render(request, "widepen/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "widepen/register.html")
